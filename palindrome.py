44fe3a195ec1e3cf17a4053d16ff5a1969bf4a3d
vignesh2k9 <vigneshselva2009@gmail.com>
2024-05-25 11:00:19 +0530

    initial Commit

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+# Mystr = "malayalam"
+#reversing a string 
+Mystr = input("Enter the String : ")
+rev = Mystr [::-1]
+if Mystr == rev:
+    print("Entered String",Mystr,"is Palindrome")
+else:
+    print("Entered String",Mystr,"is not Palindrome")
+
+
+
